586dcea9fb550721674f6e24ea543f2b6587d3cc
Pavel Rybintsev <pavel.rybintsev@validationcloud.io>
2024-06-12 17:34:18 -0700

    removed circular ref

--- x/feemarket/types/codec.go
+++ x/feemarket/types/codec.go
@@ -5,8 +5,6 @@ import (
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
-
-	_ "github.com/validationcloud/ethermint/x/evm/types"
 )
 
 var (
